005aa3b7bba2603343a4ef5054548c2abd2bb65b
Igor Pejic <igorpejicw@gmail.com>
2023-12-09 22:20:39 +0000

    fix copy paste

--- startup-programs.sh
+++ startup-programs.sh
@@ -3,4 +3,5 @@
 xmodmap ~/.Xmodmap
 xset r rate 220 40
 xbindkeys
-notify-send 'The system is ready. Enjoy.'
+./.screenlayout/home.sh
+notify-send 'The system is ready. Enjoy.' -t 2000 -i face-wink-symbolic
