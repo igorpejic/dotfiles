5931a1cbd5604dec7fd11a62be067dc3d5fb3b12
Igor Pejic <igorpejicw@gmail.com>
2024-03-10 15:43:58 +0000

    remove old config files

--- lock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Pause and unpause dunst. From the manual dunst(1):
-
-# When paused dunst will not display any notifications but keep all notifications
-# in a queue.  This can for example be wrapped around a screen locker (i3lock, slock)
-# to prevent flickering of notifications through the lock and
-# to read all missed notifications after returning to the computer.
-i3lock --nofork -i /home/igor/Pictures/dual.png
