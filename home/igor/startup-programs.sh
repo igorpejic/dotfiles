20646f814f06acf039a1f3ea93d24a87df591a14
Igor Pejic <igorpejicw@gmail.com>
2024-03-22 13:40:44 +0000

    output screenshot to tmp as well

--- home/igor/startup-programs.sh
+++ home/igor/startup-programs.sh
@@ -4,8 +4,8 @@ xmodmap ~/.Xmodmap
 xset r rate 220 40
 xbindkeys
 
-autocutsel -s PRIMARY   & 
-autocutsel -s CLIPBOARD &
+# autocutsel -s PRIMARY   &
+# autocutsel -s CLIPBOARD &
 
 sleep 1
 notify-send 'The system is ready. Enjoy.' -t 2000 -i face-wink-symbolic
